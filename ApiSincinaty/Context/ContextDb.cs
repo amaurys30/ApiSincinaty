a2c51c47225f6b36696691eb257e21e7bf78405e
amaurys30 <amaurys.castro@cecar.edu.co>
2025-05-21 11:49:43 -0500

    first commit

---
+++ ApiSincinaty/Context/ContextDb.cs
@@ -0,0 +1,16 @@
+﻿using ApiSincinaty.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace ApiSincinaty.Context
+{
+    public class ContextDb : DbContext
+    {
+        public ContextDb(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
---
+++ ApiSincinaty/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiSincinaty.Models
+{
+    public class Users
+    {
+        [Key]
+        public int user_id { get; set; }
+
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string? password { get; set; }
+        public string user_type { get; set; }
+        public string status { get; set; }
+        public string? registration_date { get; set; }
+    }
+}
